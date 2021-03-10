4748918e246ff458c30d0fee05d046cb71eea1d9
Andres Gaviria T <44479766+agavariat@users.noreply.github.com>
2021-03-10 18:57:51 -0500

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'CRM Extended',
+    'version': '13.0.0.1',
+    'category': 'CRM',
+    'description': """
+    
+""",
+    'author': 'Janbaz Aga_',
+    'website': '',
+    'images': [],
+    'depends': ['crm'],
+
+    'data': [
+        'security/crm_security.xml',
+        # 'security/ir.model.access.csv',
+        'views/crm_lead_view.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    "images": [],
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
